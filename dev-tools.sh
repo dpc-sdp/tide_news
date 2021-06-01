ab513f4b89f8ab26aa1f464b26b1a33e5f3ea888
MdNadimHossain <getovic@gmail.com>
2021-06-01 15:32:55 +1000

    [ux_change] Removed tide_core patch and dev-tools commit ref.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-export GH_COMMIT=855db37db29f66885716bfe96461f32db12d7269
+# export GH_COMMIT=COMMIT_SHA
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
